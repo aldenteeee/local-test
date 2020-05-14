f094da3eabbcdd042c5a15c3482804ffffc9b4f4
t-haruki <60593771+t-haruki@users.noreply.github.com>
2020-05-14 20:40:24 +0900

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const basicAuth = require('basic-auth-connect');
+const path = require('path');
+
+const app = express();
+app.all('/*', basicAuth(function(user, password) {
+  return user === '' && password === '';
+}));
+app.use(express.static(path.resolve(__dirname, '../html/https/')));
+
+exports.app = functions.https.onRequest(app);
