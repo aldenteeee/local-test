4f449834058817642bed7aab27a05d88c2f5e1d9
aldenteeee <m270th@gmail.com>
2020-05-20 12:08:17 +0900

    Update index.js

--- staging/html/https/index.js
+++ staging/html/https/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 app.all('/*', basicAuth(function(user, password) {
-  return user === 'abc' && password === 'abc';
+  return user === '' && password === '';
 }));
 app.use(express.static('./src/'));
 
